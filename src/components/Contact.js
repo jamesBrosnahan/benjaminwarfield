a44e4b9a86eb870189023adb6e5e90852f7fa633
Benjamin W. Smith <benjaminwarfield@just-another.net>
2016-12-21 20:02:04 -0500

    Add contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import {h} from 'preact';  /** @jsx h */
+
+export const Contact = () => (
+  <div class='content'>
+    <div class='callout'>
+      Here&#39;s the deal. If you really want to get in touch with me, you should do so in a secure way.
+    </div>
+    <div class='callout-sub'>
+     Also, if I&#39;m honest, it is more meaningful to me if you have to try a wee bit harder.
+    </div>
+  </div>
+);
