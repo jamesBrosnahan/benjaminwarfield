200cf299fc6ff2146a10a9f83ab0d707b64c0622
Benjamin W. Smith <benjaminwarfield@just-another.net>
2016-12-22 10:17:20 -0500

    Work on nav collapse

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,15 +5,22 @@ export const Navigation = () => (
   <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
-         <Link class="navbar-brand" href="/">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <Link class="navbar-brand" href="/">
           @benjaminws
-         </Link>
+        </Link>
       </div>
-      <div class="collapse navbar-collapse">
+
+      <div class="collapse navbar-collapse" id="navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
-          <li><Link href="/work">Work</Link></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact"> Contact</a></li>
+          <li><a href="/work">Work</a></li>
         </ul>
       </div>
     </div>
