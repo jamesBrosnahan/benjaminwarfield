173c7d6a3a1275290773ae5750ab2d30e6402db6
Benjamin W. Smith <benjaminws@users.noreply.github.com>
2016-12-24 00:53:39 -0500

    Deployments (#1)
    * Add travis setup for deployment
    
    * only test latest node
    
    * Add release script. Pass on tests for now.
    
    * Disable test run
    
    * Add basic caching
    
    * Update release code with latest. GIVE PROPER ATTRIBUTION
    
    * bump
    
    * Fix env vars
    
    * Fix up secure var
    
    * bump
    
    * Build status
    
    * Updates in the wrong branch
    
    * Bump
    
    * Add deploy script.
    
    * bump
    
    * Fix link to facebook
    
    * Start abstracting the deploy script.
    
    * Update deploy things
    
    - Add deploy script wrapper
    - Update keys
    
    * Bump to test
    
    * whitespace lol
    
    * Use later node versions
    
    * bump

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+/* eslint-disable strict, no-console */
+'use strict';
+
+const exec = require('child_process').exec;
+const CF_DISTRIBUTION = process.env.CF_DISTRIBUTION || '';
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '';
+const REGION = process.env.REGION || 'us-east-2'
+const BUCKET = process.env.BUCKET || '';
+const VERSION = process.env.TRAVIS_TAG || new Date().toISOString();
+
+const runDeploy = () => {
+  const cmd = `./node_modules/.bin/react-deploy-s3 \
+    --access-key-id ${AWS_ACCESS_KEY_ID} \
+    --secret-access-key ${AWS_SECRET_ACCESS_KEY} \
+    --bucket ${BUCKET} \
+    --region ${REGION} \
+    --distribution-id ${CF_DISTRIBUTION}`;
+
+  console.log(`Deploying ${VERSION}...`);
+
+  exec(cmd, (error, stdout, stderr) => {
+    console.log('Deployment complete.')
+    if (error !== null) {
+      console.log(`Error: ${error} stdout: ${stdout} stderr: ${stderr}`);
+    }
+  });
+};
+
+runDeploy();
--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,7 @@ import {h} from 'preact';  /** @jsx h */
 
 export const Work = () => (
   <div class='content'>
-    <div class='callout'>Let&#39;s talk about work.</div>
+    <div class='callout'>Let&#39;s talk about work. I&#39;ve been part of some special teams, that have build some amazing things.</div>
+    <div class='callout-sub'>That doesn&#39;t mean I can&#39;t help you.</div>
   </div>
 );
