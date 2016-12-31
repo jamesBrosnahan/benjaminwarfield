8ce9daf88a0728c352a5bab16288feab6b60c68c
Benjamin W. Smith <benjaminws@users.noreply.github.com>
2016-12-30 20:31:40 -0500

    Home updates (#5)
    * Home page updates
    
    - Add heading to home page
    - Wrap quote in a p
    - Give quote some margins
    
    * Tone down the margins, fam

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,12 @@ import {h} from 'preact';  /** @jsx h */
 import {Link} from 'preact-router';
 
 export const Home = () => (
-  <div class='content'>
+  <div>
+   <h1>Hello</h1>
+   <div class='content'>
     <div class='callout'>
-      &ldquo;Creating a website about yourself feels kind of like when you were a kid and had to move schools; suddenly you find yourself standing in front of the class, explaining your weird & overly complicated story like an idiot.&rdquo; - Me (<Link href="/about">Learn More...</Link>)
+      <p>&ldquo;Creating a website about yourself feels kind of like when you were a kid and had to move schools; suddenly you find yourself standing in front of the class, explaining your weird & overly complicated story like an idiot.&rdquo; - Me (<Link href="/about">Learn More...</Link>)</p>
     </div>
+   </div>
   </div>
 );
