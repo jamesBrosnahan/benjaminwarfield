20ba5e84e727363b308f44e21402bdf67518dc57
Benjamin W. Smith <benjaminwarfield@just-another.net>
2017-01-03 20:41:31 -0500

    Update error page a bit

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,6 +3,6 @@ import {h} from 'preact';  /** @jsx h */
 export const Error = ({type, url}) => (
 	<div>
 		<h1>Welp, we have a problem.</h1>
-		<h2>Error {type}: {url}</h2>
+		<div class='callout'>We got error <a href={`https://httpstatuses.com/${type}`}>{type}</a>, when trying to access the following URL: <br />{url}</div>
 	</div>
 );
